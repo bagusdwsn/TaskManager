e5235395b2241d8516bdb6ce6acf072d72d2a54c
Bagus Dwi Santoso <77262301+baguscodes@users.noreply.github.com>
2024-03-14 15:01:18 +0700

    Perubahan Menu

--- main.py
+++ main.py
@@ -14,36 +14,36 @@ def main():
 
     while True:
         print("\nTask Manager Menu:")
-        print("1. View tasks")
-        print("2. Add task")
-        print("3. Complete task")
-        print("4. Delete completed tasks")
-        print("5. Save and Exit")
+        print("1. Lihat Tugas")
+        print("2. Tambah Tugas")
+        print("3. Tandai Selesai")
+        print("4. Hapus Tugas Selesai")
+        print("5. Simpan dan Keluar")
 
-        choice = input("Enter your choice: ")
+        choice = input("Pilih Menu: ")
 
         if choice == "1":
             tasks = task_manager.list_tasks()
             print_tasks(tasks)
         elif choice == "2":
-            task_name = input("Enter task name: ")
+            task_name = input("Masukkan Tugas: ")
             task_manager.add_task(task_name)
-            print("Task added successfully.")
+            print("Berhasil Ditambahkan.")
         elif choice == "3":
             tasks = task_manager.list_tasks()
             print_tasks(tasks)
-            task_index = int(input("Enter task number to mark as complete: ")) - 1
+            task_index = int(input("Masukkan Nomor Tugas Selesai: ")) - 1
             if 0 <= task_index < len(tasks):
                 task_manager.complete_task(task_index)
-                print("Task marked as complete.")
+                print("Tugas Ditandai Selesai.")
             else:
                 print("Invalid task number.")
         elif choice == "4":
             task_manager.delete_completed_tasks()
-            print("Completed tasks deleted.")
+            print("Tugas Selesai Dihapus!.")
         elif choice == "5":
-            task_manager.save_tasks("data/tasks.json")  # Save tasks to file
-            print("Tasks saved. Exiting...")
+            task_manager.save_tasks("data/tasks.json")
+            print("Tugas Tersimpan. Bye...")
             break
         else:
             print("Invalid choice. Please enter a number from the menu.")
